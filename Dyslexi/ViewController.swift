b34ece924048b1a5e73707c2782e796a63ca623e
kanandev2024 <kananramazandev@gmail.com>
2024-03-19 18:17:19 +0530

    Initial Commit

---
+++ Dyslexi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dyslexi
+//
+//  Created by Veerjyot Singh on 19/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
